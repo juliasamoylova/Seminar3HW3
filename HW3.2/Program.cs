9ff8a5cec63f008bb2400c385e49495e96590c62
Juliasamoylova <julia.g.samoylova@gmail.com>
2022-07-06 10:33:26 +0300

    HW3

---
+++ HW3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+int ax = Convert.ToInt32(Console.ReadLine());
+int ay = Convert.ToInt32(Console.ReadLine());
+int az = Convert.ToInt32(Console.ReadLine());
+int bx = Convert.ToInt32(Console.ReadLine());
+int by = Convert.ToInt32(Console.ReadLine());
+int bz = Convert.ToInt32(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+
+Console.WriteLine("Расстояние между точками A и B равно: " + Math.Round(dist, 2));
+
